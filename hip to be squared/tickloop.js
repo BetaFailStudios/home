b36d401c4eb0150aa949e11c4a6bb63c89583b5b
BetaFailStudios <betafailstudios@gmail.com>
2025-12-02 19:41:14 -0600

    Update tickloop.js

--- hip to be squared/tickloop.js
+++ hip to be squared/tickloop.js
@@ -250,10 +250,11 @@ function tickloop() {
     ctx.beginPath();
     ctx.fillStyle = "#000";
     ctx.font = "25px share tech";
-    ctx.fillText("Version: b.0.6.7",700,470);
+    ctx.fillText("Version: b.0.6.8",700,470);
 }
 
 setInterval( tickloop, 1000/60 );
 
 //enemies.push(new Enemy(enemyBlueprints[4],{x:600}));
+
 //game.musicPos = 1;
