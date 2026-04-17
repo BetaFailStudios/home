948171a2c8652baf4a29d3b4fc2a39dc24276060
BetaFailStudios <betafailstudios@gmail.com>
2026-04-17 15:01:53 -0500

    Update draw.js

--- hip to be squared/draw.js
+++ hip to be squared/draw.js
@@ -17,9 +17,9 @@ const calculatePoints = gpu.createKernel(function(groups,groupsLength,musicWobbl
 }, {
   dynamicOutput: true,
   dynamicArguments: true
-}).setOutput([200,500]);
+}).setOutput([200,200]);
 
-const highest = [100,30];
+let highestAmount = 200;
 
 let toDraw = [];
 
@@ -231,176 +231,16 @@ function __draw(path) {
     //ctx.restore();
 }
 
-function drawGroup(path, groups,drawOutline) {
-    return;
-    const path2 = [];
-    path.forEach( item => {
-        if (item.type == "point") {
-            path2.push( [ item.x, item.y ] );
-        } else {
-            path2.push( [ 0, 0 ] );
-        }
-    } )
-
-    if (path2.length > highest[0] || groups.length > highest[1]) {
-        calculatePoints.setOutput([2,path2.length,groups.length]);
-        highest[0] = path2.length;
-        highest[1] = groups.length;
-    }
-    const points = calculatePoints(path2,groups,path2.length,groups.length);
-
-    let firstStroke = true;
-    let stop = -1;
-    let nextStop = -1;
-    let stroke = false;
-
-    ctx.beginPath();
-    if (groups[0][4] !== undefined) ctx.globalAlpha = groups[0][4];
-    while (stop < path.length-3) {
-        stop = nextStop;
-        for (var i = 0; i < groups.length; i++) {
-            let move = true;
-            for(var i2 = stop+1; i2 < path.length; i2++) {
-                const item = path[i2];
-                switch(item.type) {
-                    case "point": {
-                        if (item.move || move) {
-                            ctx.moveTo(...points[i][i2]);
-                            move = false;
-                        }
-                        else ctx.lineTo(...points[i][i2]);
-                        break;
-                    }
-                    case "fill": {
-                        if (i) break;
-                        nextStop = i2;
-                        i2 = path.length;
-                        ctx.fillStyle = "rgb("+item.r+","+item.g+","+item.b+")";
-                        stroke = false;
-                        break;
-                    }
-                    case "stroke": {
-                        if (i) break;
-                        nextStop = i2;
-                        i2 = path.length;
-                        ctx.strokeStyle = "rgb("+item.r+","+item.g+","+item.b+")";
-                        stroke = true;
-                        move = true;
-                        break;
-                    }
-                    case "close": {
-                        move = true;
-                        ctx.closePath();
-                        break;
-                    }
-                }
-            }
-            ctx.closePath();
-        }
-
-        if (stroke) {
-            ctx.stroke();
-            ctx.beginPath();
-        } else {
-            if (drawOutline && firstStroke) {
-                ctx.lineWidth = 7;
-                ctx.strokeStyle = "rgb(250,20,20)";
-                ctx.stroke();
-                ctx.lineWidth = 3;
-                firstStroke = false;
-            }
-            ctx.fill();
-
-            if (false && path[stop+1].type == "stroke") {
-                ctx.strokeStyle = "rgb("+item.r+","+item.g+","+item.b+")";
-                ctx.stroke();
-                ctx.beginPath();
-                stop++;
-                nextStop++;
-            }
-        }
-    }
-
-    if (groups[0][4] !== undefined) ctx.globalAlpha = 1
-    
-    //ctx.fillStyle = "#000";
-    //ctx.fill();
-    return;
-    if (groups[0][4] !== undefined && groups[0][4] !== false) ctx.globalAlpha = groups[0][4];
-    while (stop < path.length-1) {
-        stop = nextStop;
-        groups.forEach((group, g) => {
-            const ratio = (group[2]+game.musicWobble)/250;
-            if (typeof group[3] !== "object") {
-                group[3] = [Math.cos(group[3] || 0),Math.sin(group[3] || 0)];
-            }
-            //ctx.save();
-            //ctx.translate(group[0],group[1]);
-            //if (group[3]) ctx.rotate(group[3]);
-
-            let move = true;
-            for(var i = stop + 1; i < path.length; i++) {
-                const item = path[i];
-                switch(item.type) {
-                    case "point": {
-                        if (item.move || move) {
-                            ctx.moveTo(group[0]+(item.x*group[3][0]-item.y*group[3][1])*ratio,group[1]+(item.y*group[3][0]+item.x*group[3][1])*ratio);
-                            move = false;
-                        }
-                        else ctx.lineTo(group[0]+(item.x*group[3][0]-item.y*group[3][1])*ratio,group[1]+(item.y*group[3][0]+item.x*group[3][1])*ratio);
-                        break;
-                    }
-                    case "fill": {
-                        if (g) break;
-                        nextStop = i;
-                        i = path.length;
-                        ctx.fillStyle = "rgb("+item.r+","+item.g+","+item.b+")";
-                        stroke = false;
-                        //ctx.fill();
-                        break;
-                    }
-                    case "stroke": {
-                        if (g) break;
-                        nextStop = i;
-                        i = path.length;
-                        ctx.strokeStyle = "rgb("+item.r+","+item.g+","+item.b+")";
-                        stroke = true;
-                        move = true;
-                        break;
-                    }
-                    case "close": {
-                        ctx.closePath();
-                        break;
-                    }
-                }
-            }
-        
-            //if (group[3]) ctx.rotate(-group[3]);
-            //ctx.translate(-group[0],-group[1]);;
-        })
-
-        if (stroke) {
-            ctx.stroke();
-            ctx.beginPath();
-        } else {
-            if (drawOutline && firstStroke) {
-                ctx.lineWidth = 7;
-                ctx.strokeStyle = "rgb(250,20,20)";
-                ctx.stroke();
-                ctx.lineWidth = 3;
-                firstStroke = false;
-            }
-            ctx.fill();
-        }
-    }
-
-    if (groups[0][4] !== undefined) ctx.globalAlpha = 1
-
-    //ctx.restore();
-}
-
 function drawAll() {
-    if (!toDraw.length) return;
+    if (toDraw.length > highestAmount) {
+        highestAmount = toDraw.length;
+        calculatePoints.setOutput([200,highestAmount]);
+    }
+    else if (toDraw.length < highestAmount-200) {
+        highestAmount = Math.max(200,toDraw.length);
+        calculatePoints.setOutput([200,highestAmount]);
+    }
+    else if (!toDraw.length) return;
     const toDrawClone = toDraw;
     toDraw = [];
     calculatePoints(toDrawClone,toDrawClone.length,game.musicWobble).forEach((item,itemIndex) => {
